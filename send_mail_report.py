2d57c056c575f7a08fa5862ead79b6dfe9caecc6
r3drun3 <simone.ragonesi@sighup.io>
2024-01-10 20:17:46 +0100

    ci: test
    Signed-off-by: r3drun3 <simone.ragonesi@sighup.io>

--- send_mail_report.py
+++ send_mail_report.py
@@ -1,12 +1,12 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import os
 import yaml
-#import html
+import smtplib
 from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# Get the path to the YAML file
+
+# Get the path to the Github Action YAML file
 yaml_file_path = os.path.join(os.path.dirname(__file__), '.github/workflows/patch.yaml')
 
 # Read the YAML file to get the patched image list
@@ -15,17 +15,14 @@ with open(yaml_file_path, 'r') as yaml_file:
     patched_images = yaml_content.get('jobs', {}).get('immunize', {}).get('strategy', {}).get('matrix', {}).get('images', [])
 
 print("Patched images:", patched_images)
-# Get the current timestamp
-current_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+current_timestamp = datetime.now().strftime('%Y-%m-%d')
 # Prepare the HTML content
 subject = 'IMMUNIZE: Patched Image Report'
-html_body = '<h2>Patched Images 💉- {}</h2><ul>'.format(current_timestamp)
+html_body = '<h2>Patched Images 💉 {}</h2><ul>'.format(current_timestamp)
 for image in patched_images:
-    # encoded_image_name = html.escape(image)
-    github_link = f'https://github.com/r3drun3/pkgs/container/immunize/{image}'
     html_body += f'<li>{image}</li>'
 html_body += '</ul><br />'
-html_body += 'check the full catalog <a href="https://github.com/R3DRUN3?tab=packages&repo_name=immunize">here</a>'
+html_body += 'check the full catalog 📚 <a href="https://github.com/R3DRUN3?tab=packages&repo_name=immunize">here</a> !'
 
 # Get email and password from GitHub secrets
 email_address = os.environ.get('EMAIL_ADDRESS', '')
@@ -47,4 +44,4 @@ with smtplib.SMTP('smtp.gmail.com', 587) as server:
     server.login(email_address, email_password)
     server.sendmail(email_address, recipients, message.as_string())
 
-print('Email sent successfully!')
+print('Emails sent successfully!')
